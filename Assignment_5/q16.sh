1f44eb44b8cdd203b7fb5889ca61a3ed0e112301
prajwalnirmal7 <nirmalprajwal8@gmail.com>
2024-10-29 19:34:53 +0530

    Assignment_5 Question 16 Completed

---
+++ Assignment_5/q16.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Aim Write a shell script to display welcome message to the user along with contents of his home directory. Ensure that shell script will execute automatically when user login to the shell. (Make entry of your shell script into .bashrc file into your home directory).
+
+
+echo "Welcome to ubuntu -$USER"
+ls "$HOME"
